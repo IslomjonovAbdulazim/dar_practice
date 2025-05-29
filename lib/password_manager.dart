0dcbdf0ae3dae00e9df981aa0e04ba7da2287029
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-29 17:48:53 +0500

    imp

--- lib/password_manager.dart
+++ lib/password_manager.dart
@@ -3,7 +3,42 @@ import 'dart:io';
 void main() async {
   final file = File("lib/passwords.txt");
 
-  await file.writeAsString("qwerty");
   final res = await file.readAsString();
-  print(res);
+
+  String user = "";
+  while (user != res) {
+    stdout.write("Parolni kiriting: ");
+    user = stdin.readLineSync() ?? "";
+  }
+
+  while (true) {
+    print("Dasturimizga xush kelibsiz!");
+    print("1. Parolni o'zgartirish");
+    print("2. Parolni o'chirish");
+    print("0. Dasturni to'xtatish");
+    stdout.write("Tanlang: ");
+    String choice = stdin.readLineSync() ?? "";
+
+    if (choice == "0") {
+      return;
+    } else if (choice == "2") {
+      await file.writeAsString("");
+      print("Parol o'chirildi!\n");
+    } else if (choice == "1") {
+      stdout.write("Yangi Parol: ");
+      final pass = stdin.readLineSync() ?? "";
+      await file.writeAsString(pass);
+      print("Parol O'zgardi\n");
+    } else {
+      print("Noto'g'ri tanlov!\n");
+    }
+  }
 }
+
+
+
+
+
+
+
+
